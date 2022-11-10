9f441a32ded4649c0b24c4d41931c8ce74852794
Katherine Kent <k4tiek3nt1981@gmail.com>
2022-11-10 15:01:23 -0700

    Implementation of Social Authentication
    Updated routes.js to implement OAuth with GitHub. (Corrected to be inside the module instead of below. This was causing an error with being able to route for GitHub).

--- routes.js
+++ routes.js
@@ -9,16 +9,18 @@ module.exports = function (app, myDataBase) {
   app.route('/').get((req, res) => {
 
   //added to allow rendering of home page (index)
+  //updated to include showSocialAuth: true
   res.render('index', { 
     title: 'Connected to Database', 
     message: 'Please log in',
     showLogin: true,
-    showRegistration: true 
+    showRegistration: true,
+    showSocialAuth: true
     });  
   });
 
   //added to allow routing to profile page when valid user logs in
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req,   res) => {
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
 
@@ -38,41 +40,47 @@ module.exports = function (app, myDataBase) {
 
   //added /register to allow a user to register with POST request that 
   //also authenticates info to confirm if user exists or is new. 
-  app.route('/register')
-    .post((req, res, next) => {
-      //added to allow password to be hashed (encrypted)
-      const hash = bcrypt.hashSync(req.body.password, 12);
-      //query for user
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect('/');
-        } else {
-          //request to add user to database, if not found
-          myDataBase.insertOne({
-            username: req.body.username,
-            //updated from req.body.password to apply password hashing
-            password: hash
-          },
-            (err, doc) => {
-              if (err) {
-                res.redirect('/');
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
+  app.route('/register').post((req, res, next) => {
+    //added to allow password to be hashed (encrypted)
+    const hash = bcrypt.hashSync(req.body.password, 12);
+    //query for user
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        //request to add user to database, if not found
+        myDataBase.insertOne({
+          username: req.body.username,
+          //updated from req.body.password to apply password hashing
+          password: hash
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
             }
-          )
-        }
-      })
-    },
-      passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res, next) => {
-        res.redirect('/profile');
+          }
+        )
       }
-    );
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  //added to allow OAuth with github, now inside module instead of below
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback')
+    .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+      res.redirect('/profile');
+  });
   
   //added to handle 404 errors (page not found), this is middleware
   app.use((req, res, next) => {
